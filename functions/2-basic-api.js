9a272a60b94bfa602bce136d614973d1343761ab
Aljosa <aljosaa.njezic@gmail.com>
2021-06-14 23:13:20 +0200

    course error fix

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -4,6 +4,9 @@ const items = require('../assets/data')
 exports.handler = async(event,context,cb) => {
 
  return {
+  headers: {
+   'Access-Control-Allow_origin': '*'
+  },
  statusCode: 200,
   body: JSON.stringify(items),
  }
