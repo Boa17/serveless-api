765ff6b123ee8fa4da5c894b78cf22c05899d67b
Aljosa <aljosaa.njezic@gmail.com>
2021-07-21 00:25:38 +0200

    new

--- functions/4-z-complete.js
+++ functions/4-z-complete.js
@@ -5,7 +5,7 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
   .base('appyRPUiWdEB5KJ7v')
   .table('products')
 
-exports.handler = async (event, context, cb) => {
+  exports.handler = async (event, context, cb) => {
   const { id } = event.queryStringParameters
   if (id) {
     try {
@@ -27,25 +27,22 @@ exports.handler = async (event, context, cb) => {
       }
     }
   }
-try {
- const {records} = await airtable.list()
- const products = records.map((product) => {
-  const {id} = product;
-  const {name,image,price} = product.fields
-  const url = image[0].url
-  return {id,name,url,price}
- })
-  return {
- statusCode: 200,
-  body: JSON.stringify(products),
- }
-} catch (error) {
-  return {
- statusCode: 500,
-  body: 'Server Error',
- }
+  try {
+    const { records } = await airtable.list()
+    const products = records.map((product) => {
+      const { id } = product
+      const { name, image, price } = product.fields
+      const url = image[0].url
+      return { id, name, url, price }
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    }
+  }
 }
-
-}
-
-
